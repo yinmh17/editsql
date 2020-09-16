e7a81d3c6294ffc4dbccb2a68c2ade0d7cedeb77
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2020-09-16 16:45:07 +0800

    Update postprocess_eval.py

--- postprocess_eval.py
+++ postprocess_eval.py
@@ -417,7 +417,7 @@ def write_and_evaluate(postprocess_sqls, db_path, table_schema_path, gold_path, 
         for postprocess_sql, interaction_id, turn_id in postprocess_sqls[db]:
           f.write(postprocess_sql+'\n')
 
-    command = 'python3 eval_scripts/evaluation.py --db {} --table {} --etype match --gold {} --pred {}'.format(db_path,
+    command = 'python eval_scripts/evaluation.py --db {} --table {} --etype match --gold {} --pred {}'.format(db_path,
                                                                                                   table_schema_path,
                                                                                                   gold_path,
                                                                                                   os.path.abspath(output_file))
@@ -431,7 +431,7 @@ def write_and_evaluate(postprocess_sqls, db_path, table_schema_path, gold_path, 
           f.write('{}\n'.format(postprocess_sql))
           cnt += 1
 
-    command = 'python2 eval_scripts/evaluation_sqa.py --db {} --table {} --etype match --gold {} --pred {}'.format(db_path,
+    command = 'python eval_scripts/evaluation_sqa.py --db {} --table {} --etype match --gold {} --pred {}'.format(db_path,
                                                                                                       table_schema_path,
                                                                                                       gold_path,
                                                                                                       os.path.abspath(output_file))
